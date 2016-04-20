20ec959a57650c343f7ac6544099525f7029a2ab
Teodor <doreto95@gmail.com>
2016-04-20 16:42:35 +0300

    Pretty much final state of the game.

---
+++ src/game/Launcher.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Game game = new Game("Battle City - \"Team Darby\"", 800, 600);
+        game.start();
+    }
+}
